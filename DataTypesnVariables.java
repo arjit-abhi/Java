f478917193050e6b900508b639a84428cef0e584
Abhikalp Arjit Singh <1308singhvipul@gmail.com>
2024-12-20 20:01:16 +0530

    task 1 question 1

--- DataTypesnVariables.java
+++ DataTypesnVariables.java
@@ -1,3 +1,29 @@
 public class DataTypesnVariables {
-    
+    class InitVar{
+        public void initvar(String[] args) {
+            byte a = 2;
+            short b = 9;
+            int c = -3;
+            long d = 40909;
+            float e = 1;
+            double f = 1.01;
+            boolean g = true;
+            char h = 't';
+
+            System.out.println(a);
+            System.out.println(b);
+            System.out.println(c);
+            System.out.println(d);
+            System.out.println(e);
+            System.out.println(f);
+            System.out.println(g);
+            System.out.println(h);
+        }
+    }
+
+    public static void main(String[] args) {
+        DataTypesnVariables outerInstance = new DataTypesnVariables();
+        InitVar innerInstance = outerInstance.new InitVar();
+        innerInstance.initvar(args);
+    }
 }
